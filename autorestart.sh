cb1b8448b685dee82438f148d5ba36768e228e6d
trixt63 <xuantung.trinh.63@gmail.com>
2024-06-04 17:59:01 +0700

    chane xcape timeout

--- autorestart.sh
+++ autorestart.sh
@@ -13,7 +13,7 @@ feh --bg-fill /home/trinhtung/.config/qtile/assets/wallpapers/saudi-mountains.jp
 # autorandr --change &
 #
 #X keyboard extension
-sleep 1.5 && xkbcomp /home/trinhtung/.config/Xkeymap $DISPLAY &
+sleep 1.0 && xkbcomp /home/trinhtung/.config/Xkeymap $DISPLAY &
 killall xcape &
 xcape -e 'Control_L=Escape;'
 #
