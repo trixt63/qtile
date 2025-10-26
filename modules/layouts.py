3ad7bb4a1d54108bd406bf411ace772172f33120
trixt63 <xuantung.trinh.63@gmail.com>
2025-10-26 18:57:55 +0700

    re-add default_float_rules

--- modules/layouts.py
+++ modules/layouts.py
@@ -61,7 +61,7 @@ floating_layout = layout.Floating(
     border_width=BW-2,
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
-        # *layout.Floating.default_float_rules,
+        *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
